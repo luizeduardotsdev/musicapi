dd7f0375c247fa68d2ab178073df147283ea493f
luiz eduardo <luizeduardotsdev@gmail.com>
2026-01-24 14:01:36 -0300

    feat: cria repository para genero_musical

---
+++ src/main/java/dev/io/luizeduardotsdev/musicapi/repository/GeneroMusicalRepository.java
@@ -0,0 +1,7 @@
+package dev.io.luizeduardotsdev.musicapi.repository;
+
+import dev.io.luizeduardotsdev.musicapi.genero.GeneroMusical;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GeneroMusicalRepository extends JpaRepository<GeneroMusical, Long> {
+}
