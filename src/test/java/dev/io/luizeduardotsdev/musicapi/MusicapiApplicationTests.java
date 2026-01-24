a8de4f48ee8ad202c2df87eec076206e261afca8
luiz eduardo <luizeduardotsdev@gmail.com>
2026-01-24 13:23:00 -0300

    chore: inicia pacotes iniciais

---
+++ src/test/java/dev/io/luizeduardotsdev/musicapi/MusicapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.io.luizeduardotsdev.musicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
