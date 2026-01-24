a40a17c480c35b27f7e05633d53fdd2938ddec6b
luiz eduardo <luizeduardotsdev@gmail.com>
2026-01-24 14:23:48 -0300

    feat: add length de 50 no nome da entidade genero_musical

--- src/main/java/dev/io/luizeduardotsdev/musicapi/genero/GeneroMusical.java
+++ src/main/java/dev/io/luizeduardotsdev/musicapi/genero/GeneroMusical.java
@@ -12,6 +12,6 @@ public class GeneroMusical {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nome", nullable = false, unique = true)
+    @Column(name = "nome", nullable = false, unique = true, length = 50)
     private String nome;
 }
